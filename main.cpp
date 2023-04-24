0d3c2002ca50a9502227b3caa2ed8907670b2354
lolamackenzie <57155012+lolamackenzie@users.noreply.github.com>
2023-04-24 16:31:42 -0400

    update1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by lola on 4/24/23.
+//
+#include <fstream>
+#include <iostream>
+#include "main.h"
+using namespace std;
+
+void print_menu(){
+    cout<< "welcome to the women in the workplace simulator, press any key to continue" <<endl;
+
+}
+
+int main(){
+    print_menu();
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+//
+// Created by lola on 4/24/23.
+//
+
+#ifndef WORKPLACESIM_MAIN_H
+#define WORKPLACESIM_MAIN_H
+
+    void print_menu();
+
+
+#endif //WORKPLACESIM_MAIN_H
